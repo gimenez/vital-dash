3ceb2a5ef34f03fdd6186349b6dd8262532142f7
unknown <Gabriel.Gimenes@CR-0AE0AFE600DA.explorercallcenter.com.br>
2022-11-16 14:55:03 -0300

    aleteracao da url do config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.22.79/ci';
+$config['base_url'] = 'http://192.168.22.79/vital-dash';
 
 /*
 |--------------------------------------------------------------------------
